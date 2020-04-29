e81f4fb4126769acf8dfec0675edf1dc4a6a2fae
Niraj Patel <nirajp82@yahoo.com>
2020-04-28 20:10:43 -0400

    Completed Context Sample

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+    state = { language: 'English' };
+
+    getText = () => {
+        return this.state.language === "English" ? "Select a language: " : "भाषा चुनें: ";
+    };
+
+    render() {
+
+        return (
+            <div className="ui container">
+                <div>
+                    <label> {this.getText()}</label>
+                    <i className="flag us" onClick={() => this.setState({ language: 'English' })} />
+                    <i className="flag in" onClick={() => this.setState({ language: 'Hindi' })} />
+                    <ColorContext.Provider value='red'>
+                        <LanguageContext.Provider value={this.state.language}>
+                            <UserCreate />
+                        </LanguageContext.Provider>
+                    </ColorContext.Provider>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default connect(null, null)(App);
---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    renderValue = lang => lang === 'English' ? "Submit" : "प्रस्तुत";
+
+    render() {
+        return (
+            <ColorContext>
+                {
+                    (color) =>
+                        <button className={`ui button ${color}`}>
+                            <LanguageContext.Consumer>
+                                {lang => this.renderValue(lang)}
+                            </LanguageContext.Consumer>
+                        </button>
+                }
+            </ColorContext>
+        );
+    };
+}
+
+export default Button;
